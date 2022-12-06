79f798b39e50a4a4870ca35de91198354c8368c0
Oleksandr Cherevko <oc.oleksandr.cherevko@outlook.com>
2022-12-06 19:47:49 +0200

    first commit

---
+++ Homework_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int arrayLenght;
+        int counter = 0;
+
+        Console.Write("Please, enter array lenght: ");
+        try
+        {
+            arrayLenght = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arrayLenght];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Please, enter {0} element: ", i + 1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i >= (array.Length / 2) && array[i] > 10)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Hillel C# homework 1
